0fef6a2b995e895ea7b4eac769e5e2334116d3e3
Mark Feeney <mark.feeney@gmail.com>
2013-01-03 22:49:24 -0500

    Emacs config updates
    - turn off starter-kit packages for now
    - move backup files to central place
    - various other small tweaks

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,8 +6,8 @@
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-;; Add in your own as you wish:
-(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings clojure-mode nrepl)
+;;(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings clojure-mode nrepl)
+(defvar my-packages '(clojure-mode nrepl)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
@@ -16,5 +16,17 @@
 
 (load-theme 'tango-dark t)
 
-(remove-hook 'prog-mode-hook 'esk-turn-on-hl-line-mode)
-(menu-bar-mode)
+;;(remove-hook 'prog-mode-hook 'esk-turn-on-hl-line-mode)
+(menu-bar-mode t)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(column-number-mode t)
+(show-paren-mode t)
+
+;; put all backup files in a single location to avoid clutter
+(setq backup-directory-alist `(("." . "~/.emacs.d/.saves")))
+(setq backup-by-copying t)
+(setq inhibit-startup-message t)
+
+;; use shorter keystrokes for confirmation dialogues
+(fset 'yes-or-no-p 'y-or-n-p)
