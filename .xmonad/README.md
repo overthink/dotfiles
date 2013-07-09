e55677fc1fbe8e45c277ab3fd526f146e1aad830
Mark Feeney <mark.feeney@gmail.com>
2013-07-08 22:34:46 -0400

    Big xmonad cleanup
    - remove old ubuntu 10.04 stuff
    - maybe fix dmenu flakiness with gnome-settings-daemon (via sleep...)
    - remove tray stuff I never use (volume, nm-applet)

---
+++ .xmonad/README.md
@@ -0,0 +1,3 @@
+- Copy `xmonad-mark.desktop` to /usr/share/xsessions
+- symlink `.xmobarrc` in `$HOME`
+
--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,1 +1,36 @@
-xmonad.hs.new
+import System.IO
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.SetWMName
+import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Hooks.ICCCMFocus
+import XMonad.Hooks.EwmhDesktops
+
+myWorkspaces = ["1","2","3","4","5","6","7","8","9"]
+
+myManageHook = composeAll
+    [ className =? "Gimp"      --> doFloat
+    , className =? "Vncviewer" --> doFloat
+    ]
+
+main = do
+    xmproc <- spawnPipe "/usr/bin/xmobar /home/mark/.xmobarrc"
+    xmonad $ ewmh defaultConfig { terminal = "urxvt" }
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , startupHook = setWMName "LG3D" -- fix for Java apps
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , logHook = do
+            takeTopFocus
+            dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+        , modMask = mod4Mask     -- Rebind Mod to the Windows key
+        , workspaces = myWorkspaces
+        } `additionalKeys`
+        [ ((mod4Mask .|. shiftMask, xK_l), spawn "gnome-screensaver-command --lock")
+        --, ((mod4Mask, xK_P), spawn "dmenu_run")
+        ]
+
