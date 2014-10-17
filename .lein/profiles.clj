b2b2dd2db45549dcca8ac61a5522ccae9dba493c
Mark Feeney <mark.feeney@gmail.com>
2014-10-17 16:49:40 -0400

    Add eastwood to lein

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -3,5 +3,8 @@
         ;;:repl-options {:nrepl-middleware [lighttable.nrepl.handler/lighttable-ops]}
         :plugins [[cider/cider-nrepl "0.7.0"]
                   [lein-kibit "0.0.8"]
+                  [jonase/eastwood "0.1.4"]
                   [lein-deps-tree "0.1.2"]
-                  [lein-try "0.2.0"]]}}
+                  [lein-try "0.2.0"]]}
+        :eastwood {:exclude-linters [:wrong-arity]
+                   :add-linters [:unused-namespaces]}}
