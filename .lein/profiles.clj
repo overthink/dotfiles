4ea4b94182f8ae427bad9f8a9dd1a2064397948f
Mark Feeney <mark.feeney@gmail.com>
2014-10-29 10:22:22 -0400

    Add no.disassemble and criterium + formatting

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,10 +1,10 @@
-{:user {
-        ;;:dependencies [[lein-light-nrepl "0.0.6"]]
-        ;;:repl-options {:nrepl-middleware [lighttable.nrepl.handler/lighttable-ops]}
-        :plugins [[cider/cider-nrepl "0.7.0"]
+{:user {:plugins [[cider/cider-nrepl "0.7.0"]
                   [lein-kibit "0.0.8"]
                   [jonase/eastwood "0.1.4"]
                   [lein-deps-tree "0.1.2"]
-                  [lein-try "0.2.0"]]}
-        :eastwood {:exclude-linters [:wrong-arity]
-                   :add-linters [:unused-namespaces]}}
+                  [lein-nodisassemble "0.1.3"]
+                  [lein-try "0.2.0"]]
+        :dependencies [[criterium "0.4.3"]]}
+ :eastwood {:exclude-linters [:wrong-arity]
+            :add-linters [:unused-namespaces]}}
+
