b7ab052b871d1d774fda63adfbb6ee5525fa6927
Mark Feeney <mark.feeney@gmail.com>
2014-11-06 12:37:13 -0500

    Misc updates; no more clickable links in urxvt
    If you ever echo a huge (many MB) line to the terminal in urxvt with
    clickable links going you're going to have a bad time (pegged cpu).

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,9 +1,8 @@
 {:user {:plugins [[cider/cider-nrepl "0.7.0"]
                   [lein-kibit "0.0.8"]
                   [jonase/eastwood "0.1.4"]
-                  [lein-deps-tree "0.1.2"]
-                  [lein-nodisassemble "0.1.3"]
-                  [lein-try "0.2.0"]]
+                  ;[lein-nodisassemble "0.1.3"]
+                  [lein-try "0.4.3"]]
         :dependencies [[criterium "0.4.3"]]}
  :eastwood {:exclude-linters [:wrong-arity]
             :add-linters [:unused-namespaces]}}
--- .vimrc
+++ .vimrc
@@ -46,7 +46,8 @@ syntax enable                     " use syntax hilighting
 if !has("gui_running")
   "let g:rehash256 = 1  " use in-dev 256 color molokai
 endif
-colorscheme molokai
+"colorscheme molokai
+colorscheme ps_color
 "set guifont=Envy\ Code\ R:11:cDEFAULT,ProFontWindows:h10:cANSI,Lucida_Console:h10:cANSI,Courier_New:h10:cANSI
 set guifont=Envy\ Code\ R
 "set guifont=ProFont\ 11
