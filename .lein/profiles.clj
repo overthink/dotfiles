85f9d688f790cc70dd5226936a94757447bc5f0d
Mark Feeney <mark.feeney@gmail.com>
2015-11-19 12:39:08 -0500

    vim and lein updates

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,9 +1,9 @@
 {:user {:plugins [[cider/cider-nrepl "0.8.2"]
                   [lein-kibit "0.0.8"]
-                  [jonase/eastwood "0.2.1"]
+                  [jonase/eastwood "0.2.2"]
                   ;[lein-nodisassemble "0.1.3"]
-                  [lein-try "0.4.3"]]
-        :dependencies [[criterium "0.4.3"]]}
+                  [lein-try "0.4.3"]]}
+        ;:dependencies [[criterium "0.4.3"]]}
  :eastwood {:exclude-linters [:wrong-arity]
             :add-linters [:unused-namespaces]}}
 
