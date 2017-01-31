5607bb85569ce657b9e23025579ea778d8d78c3c
Mark Feeney <mark.feeney@gmail.com>
2017-01-31 13:09:47 -0500

    Updates to profiles.clj
    Move linters into lint profile.
    Add slamhound

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,10 +1,13 @@
-{:user {:plugins [[cider/cider-nrepl "0.9.1"]
-                  [lein-kibit "0.1.2"]
+{:lint {:plugins [[lein-kibit "0.1.3"]
                   [jonase/eastwood "0.2.3"]
-                  ;[lein-bikeshed "0.3.0"]
+                  [lein-bikeshed "0.4.1"]]
+        :eastwood {:add-linters [:unused-namespaces
+                                 :unused-locals
+                                 :keyword-typos]}}
+ :user {:plugins [[cider/cider-nrepl "0.9.1"]
                   ;[lein-nodisassemble "0.1.3"]
-                  [lein-try "0.4.3"]]}}
-        ;:dependencies [[criterium "0.4.3"]]}
-; :eastwood {:exclude-linters [:wrong-arity]
-;            :add-linters [:unused-namespaces]}}
+                  [lein-try "0.4.3"]]
+        :dependencies [#_[criterium "0.4.3"]
+                       [slamhound "1.5.5"]]
+        :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
 
